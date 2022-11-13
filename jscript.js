ec6ef9cac279b8ef14b8af1e0688caa50f54cf53
Alita-oss <111301275+Alita-oss@users.noreply.github.com>
2022-11-13 18:01:10 +0100

    new media folders

--- jscript.js
+++ jscript.js
@@ -1,11 +1,47 @@
 window.onload = function() {
+    const mainSongList = [
+        {
+            id: 'song-1',
+            title: 'Money Trees',
+            imageSrc: 'images/money_trees_kendrick.jpg',
+            imageAlt: 'Kendrick Lamer',
+            audioSrc: '',
+            artist: 'Kendrick Lamar & Jay Rock',
+        },
+        {
+            id: 'song-1',
+            title: 'Money Trees',
+            imageSrc: 'images/money_trees_kendrick.jpg',
+            imageAlt: 'Kendrick Lamer',
+            audioSrc: '',
+            artist: 'Kendrick Lamar & Jay Rock',
+        },
+        {
+            id: 'song-1',
+            title: 'Money Trees',
+            imageSrc: 'images/money_trees_kendrick.jpg',
+            imageAlt: 'Kendrick Lamer',
+            audioSrc: '',
+            artist: 'Kendrick Lamar & Jay Rock',
+        },
+    ];
+   
     const songElements = document.querySelectorAll('li');
     
     const onSongClick = ($event) => {
-        console.log($event);
+        const path = $event.path;
+        let songId;
+        
+        for (let i = 0, j = path.length; i < j; i++) {
+            if (path[i].tagName == 'LI') {
+                songId = path[i].id;
+            }
+        }
     };
 
     for (let i = 0, j = songElements.length; i < j; i++) {
         songElements[i].addEventListener('click', onSongClick, false);
     }
+
+
 };
